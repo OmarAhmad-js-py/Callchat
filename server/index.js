2780b2d863ac82d9be50df147b4eeeb160e9a98c
Wasted-O453 Ahmad <omar.dreke654@gmail.com>
2022-10-27 22:57:17 +0200

    Ruff work on the chat box and quality of life changes on the Room

--- server/index.js
+++ server/index.js
@@ -16,25 +16,44 @@ const rooms = {};
 app.use(cors())
 
 io.on("connection", socket => {
-
     socket.on("connect_error", (err) => {
         console.log(`connect_error due to ${err.message}`);
     });
     socket.on("join room", roomID => {
-        console.log("connected to room: " + roomID);
-        if (!rooms[roomID]) {
-            rooms[roomID] = [socket.id];
+        if (rooms[roomID]) {
+            rooms[roomID].push(socket.id);
+            console.log(rooms);
         }
-        rooms[roomID].push(socket.id);
-
-        console.log("Pushed")
-
+        else {
+            rooms[roomID] = [socket.id];
+            console.log(rooms);
+        }
         const otherUser = rooms[roomID].find(id => id !== socket.id);
         if (otherUser) {
             socket.emit("other user", otherUser);
             socket.to(otherUser).emit("user joined", socket.id);
         }
     });
+
+
+    socket.on("Send_message", payload => {
+        try {
+            //check which room the socket id belongs to 
+            const roomID = Object.keys(rooms).find(key => rooms[key].includes(socket.id));
+            console.log(roomID ? true : false, socket.id + " belongs to room " + Object.keys(rooms).find(key => rooms[key]));
+            if (roomID) {
+                const otherusers = Object.keys(rooms).find(key => rooms[key])
+                io.to(otherusers).emit("create message", payload.message);
+            } else {
+                console.log("Room not found")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+
+
 
     socket.on("offer", payload => {
         io.to(payload.target).emit("offer", payload);
